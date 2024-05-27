823b26257c86fef878710d0c24b9d1652eb269b5
Luan Tran <tranminhluan1206@gmail.com>
2024-05-27 23:51:22 +0700

    draft

--- gormschema/gorm.go
+++ gormschema/gorm.go
@@ -26,13 +26,21 @@ type (
 	}
 	// Option configures the Loader.
 	Option func(*Loader)
-	// ViewOption configures a viewBuilder.
-	ViewOption func(*viewBuilder)
+	// schemaOption configures the schemaBuilder.
+	schemaOption func(*schemaBuilder)
+	// ViewOption is contract for view options.
+	ViewOption interface {
+		createViewStmt(*schemaBuilder)
+	}
+	// TriggerOption is contract for trigger options.
+	TriggerOption interface {
+		createTriggerStmt(*schemaBuilder)
+	}
 	// ViewDefiner defines a view.
 	ViewDefiner interface {
 		ViewDef(dialect string) []ViewOption
 	}
-	viewBuilder struct {
+	schemaBuilder struct {
 		db         *gorm.DB
 		createStmt string
 		// viewName is only used for the BuildStmt option.
@@ -66,26 +74,34 @@ func New(dialect string, opts ...Option) *Loader {
 	return l
 }
 
+func (s schemaOption) createViewStmt(b *schemaBuilder) {
+	s(b)
+}
+func (s schemaOption) createTriggerStmt(b *schemaBuilder) {
+	s(b)
+}
+
 // CreateStmt accepts raw SQL to create a CREATE VIEW statement.
-func CreateStmt(stmt string) ViewOption {
-	return func(b *viewBuilder) {
-		b.createStmt = b.db.ToSQL(func(tx *gorm.DB) *gorm.DB {
-			return tx.Exec(stmt)
-		})
-	}
+func CreateStmt(stmt string) interface {
+	ViewOption
+	TriggerOption
+} {
+	return schemaOption(func(b *schemaBuilder) {
+		b.createStmt = stmt
+	})
 }
 
 // BuildStmt accepts a function with gorm query builder to create a CREATE VIEW statement.
 // With this option, the view's name will be the same as the model's table name
 func BuildStmt(fn func(db *gorm.DB) *gorm.DB) ViewOption {
-	return func(b *viewBuilder) {
+	return schemaOption(func(b *schemaBuilder) {
 		vd := b.db.ToSQL(func(tx *gorm.DB) *gorm.DB {
 			return fn(tx).
 				Unscoped(). // Skip gorm deleted_at filtering.
 				Find(nil)   // Execute the query and convert it to SQL.
 		})
 		b.createStmt = fmt.Sprintf("CREATE VIEW %s AS %s", b.viewName, vd)
-	}
+	})
 }
 
 // Load loads the models and returns the DDL statements representing the schema.
@@ -160,6 +176,9 @@ func (l *Loader) Load(models ...any) (string, error) {
 		return "", fmt.Errorf("unexpected migrator type: %T", db.Migrator())
 	}
 	if err = cm.CreateViews(views); err != nil {
+		return "", err
+	}
+	if err = cm.CreateTriggers(models); err != nil {
 		return "", err
 	}
 	if !l.config.DisableForeignKeyConstraintWhenMigrating && l.dialect != "sqlite" {
@@ -246,15 +265,35 @@ func (m *migrator) CreateViews(views []ViewDefiner) error {
 		}); ok {
 			viewName = namer.TableName()
 		}
-		viewBuilder := &viewBuilder{
+		schemaBuilder := &schemaBuilder{
 			db:       m.DB,
 			viewName: viewName,
 		}
 		for _, opt := range view.ViewDef(m.Dialector.Name()) {
-			opt(viewBuilder)
+			opt.createViewStmt(schemaBuilder)
 		}
-		if err := m.DB.Exec(viewBuilder.createStmt).Error; err != nil {
+		if err := m.DB.Exec(schemaBuilder.createStmt).Error; err != nil {
 			return err
+		}
+	}
+	return nil
+}
+
+// CreateTriggers creates the triggers for the given models.
+func (m *migrator) CreateTriggers(models []any) error {
+	for _, model := range models {
+		if md, ok := model.(interface {
+			Triggers(string) []TriggerOption
+		}); ok {
+			for _, option := range md.Triggers(m.Dialector.Name()) {
+				schemaBuilder := &schemaBuilder{
+					db: m.DB,
+				}
+				option.createTriggerStmt(schemaBuilder)
+				if err := m.DB.Exec(schemaBuilder.createStmt).Error; err != nil {
+					return err
+				}
+			}
 		}
 	}
 	return nil
--- internal/testdata/models/user.go
+++ internal/testdata/models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 
 	"ariga.io/atlas-provider-gorm/gormschema"
@@ -23,5 +25,25 @@ func (WorkingAgedUsers) ViewDef(dialect string) []gormschema.ViewOption {
 		gormschema.BuildStmt(func(db *gorm.DB) *gorm.DB {
 			return db.Model(&User{}).Where("age BETWEEN 18 AND 65").Select("name, age")
 		}),
+	}
+}
+
+type UserPetHistory struct {
+	UserID    uint
+	PetID     uint
+	CreatedAt time.Time
+}
+
+func (User) Triggers(dialect string) []gormschema.TriggerOption {
+	return []gormschema.TriggerOption{
+		gormschema.CreateStmt(`
+			CREATE TRIGGER user_pet_history_trigger
+			AFTER INSERT ON pets
+			FOR EACH ROW
+			BEGIN
+				INSERT INTO user_pet_histories (user_id, pet_id, created_at) 
+				VALUES (NEW.user_id, NEW.id, NOW(3));
+			END
+		`),
 	}
 }
